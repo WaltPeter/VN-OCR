0ed2fc6f488e1b746090e9be04601506b9038992
WaltPeter <tanszean2000@gmail.com>
2020-09-30 18:27:39 +0800

    Python

---
+++ Python/Lib/site-packages/bdtranslate/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+from .translate import translate
+
+__all__ = ["translate"]
---
+++ Python/Lib/site-packages/cv2/version.py
@@ -0,0 +1,4 @@
+opencv_version = "3.4.10.37"
+contrib = False
+headless = False
+ci_build = True
